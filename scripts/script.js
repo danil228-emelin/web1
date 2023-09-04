ba671bcc01e7f5140be7eff6188ab30a381952a0
danil emelin <danil228aeedj@mail.ru>
2023-09-04 16:09:18 +0300

    add JQUERY

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,30 @@
 const buttons = document.querySelectorAll("button[class=b]");
 const checkBoxes = document.querySelectorAll("input[name=num]");
 const inputY = document.querySelector("input[type=text]");
-let z;
-let x;
-let y;
+const SEND_BUTTON = document.querySelector("button[name=send]")
+const LEFT_BORDER = -5;
+const RIGHT_BORDER = 3;
+let z = null;
+let x = null;
+let y = null;
 const REQ_INT = new RegExp('^-?[0-5]*$')
-inputY.addEventListener('input', () => {
-    y = inputY.value
-    if (!REQ_INT.test(y)) {
-        inputY.value = "";
-        alert(y.concat(" :incorrect Input for y"));
-    }
 
+
+inputY.addEventListener('input', () => {
+    let res = inputY.value
+    if (res === "") {
+        return;
+    }
+    if (!REQ_INT.test(res)) {
+        inputY.value = "";
+        alert(res.concat(" :incorrect Input for y"));
+    }
+    if (res < LEFT_BORDER || res > RIGHT_BORDER) {
+        alert("value ".concat(res).concat(" out of range"));
+        inputY.value = "";
+    }
+    y = inputY.value;
+    console.log(y)
 
 })
 
@@ -22,13 +35,40 @@ checkBoxes.forEach(box => {
             if (item !== box) item.checked = false
         })
         x = box.value;
+        console.log(x)
+
     })
 })
 buttons.forEach(b => {
     b.addEventListener('click', () => {
         z = b.value
+        console.log(z)
+
     })
 })
+
+
+SEND_BUTTON.addEventListener('click', () => {
+        if (z === null || x === null || y === null) {
+            alert("Fill all the data")
+            return;
+        }
+
+        $.post("\\scripts\\server.php", {
+                x: x,
+                y: y,
+                z: z,
+                currentTime: Date.now()
+            },
+            () => {
+                console.log("WE DONE IT")
+            }
+        )
+
+    }
+)
+
+
 
 
 
