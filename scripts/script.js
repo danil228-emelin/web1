5842a809bf06830ed1c71a6d3819c28fcc9f3bb4
danil emelin <danil228aeedj@mail.ru>
2023-09-07 19:06:12 +0300

    release 1.0

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,21 @@ const inputY = document.querySelector("input[type=text]");
 const SEND_BUTTON = document.querySelector("button[name=send]")
 const LEFT_BORDER = -5;
 const RIGHT_BORDER = 3;
-let z = null;
+const table = document.getElementById("results");
+let r = null;
 let x = null;
 let y = null;
 const REQ_INT = new RegExp('^-?[0-5]*$')
 
-
+addEventListener("load", () => {
+    inputY.value = "";
+    checkBoxes.forEach(box => {
+        box.checked = false;
+    })
+    r = null;
+    x = null;
+    y = null;
+})
 inputY.addEventListener('input', () => {
     let res = inputY.value
     if (res === "") {
@@ -24,7 +33,7 @@ inputY.addEventListener('input', () => {
         inputY.value = "";
     }
     y = inputY.value;
-    console.log(y)
+    console.log("y=".concat(y))
 
 })
 
@@ -35,33 +44,55 @@ checkBoxes.forEach(box => {
             if (item !== box) item.checked = false
         })
         x = box.value;
-        console.log(x)
+        console.log("x=".concat(x));
 
     })
 })
 buttons.forEach(b => {
     b.addEventListener('click', () => {
-        z = b.value
-        console.log(z)
+        r = b.value
+        console.log("r=".concat(r));
 
     })
 })
 
 
 SEND_BUTTON.addEventListener('click', () => {
-        if (z === null || x === null || y === null) {
-            alert("Fill all the data")
+        if (r === null) {
+            alert("Fill parameter r")
+            return;
+        }
+        if (x === null) {
+            alert("Fill parameter x")
+            return;
+        }
+        if (y === null) {
+            alert("Fill parameter y")
             return;
         }
 
-        $.post("\\scripts\\server.php", {
+        $.post("\\scripts\\script.php", {
                 x: x,
                 y: y,
-                z: z,
-                currentTime: Date.now()
+                r: r,
+                currentTime: msToTime(Date.now())
             },
-            () => {
-                console.log("WE DONE IT")
+            (result) => {
+                let json = JSON.parse(result)
+
+                let row = table.insertRow(1);
+                let cell1 = row.insertCell(0);
+                let cell2 = row.insertCell(1);
+                let cell3 = row.insertCell(2);
+                let cell4 = row.insertCell(3);
+                let cell5 = row.insertCell(4);
+                let cell6 = row.insertCell(5);
+                cell1.innerHTML = json.boolean;
+                cell2.innerHTML = json.request_time;
+                cell3.innerHTML =json.response_time;
+                cell4.innerHTML =json.x;
+                cell5.innerHTML =json.y;
+                cell6.innerHTML =json.r;
             }
         )
 
@@ -69,6 +100,16 @@ SEND_BUTTON.addEventListener('click', () => {
 )
 
 
+function msToTime(duration) {
+    let milliseconds = Math.floor((duration % 1000) / 100),
+        seconds = Math.floor((duration / 1000) % 60),
+        minutes = Math.floor((duration / (1000 * 60)) % 60),
+        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
 
+    return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+}
 
--- scripts/script.php
+++ scripts/script.php
@@ -1,5 +1,18 @@
 <?php
 $x = $_POST['x'];
 $y = $_POST['y'];
-echo $x ." - ". $y ."<br />";
-?>
+$r = $_POST['r'];
+$time = $_POST['currentTime'];
+
+if (($x <= $r / 2 && $y <= -$r / 2 && $x >= 0 && $y <= 0) ||
+    ($x <= -$r / 2 && $y <= -$r && $x <= 0 && $y <= 0) ||
+    ($x <= -$r && $y <= $r && $x <= 0 && $y >= 0)) {
+
+    $data = ['x' => $x, 'y' => $y, 'r' => $r, 'boolean' => 'True', 'request_time' => $time, 'response_time' => date("H:i:s.") . gettimeofday()["usec"]];
+    echo json_encode($data);
+
+} else {
+    $now = DateTime::createFromFormat('U.u', microtime(true));
+    $data = ['x' => $x, 'y' => $y, 'r' => $r, 'boolean' => 'False', 'request_time' => $time, 'response_time' => date("H:i:s.") . gettimeofday()["usec"]];
+    echo json_encode($data);
+}
