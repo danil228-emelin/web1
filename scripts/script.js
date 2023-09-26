67e4e5c3176f1014b4802096130c52c7b64375a0
danil emelin <danil228aeedj@mail.ru>
2023-09-26 14:35:11 +0300

    use axois for sending data, clear css ,change xml

--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,56 @@
 const buttons = document.querySelectorAll("button[class=b]");
 const checkBoxes = document.querySelectorAll("input[name=num]");
 const inputY = document.querySelector("input[type=text]");
-const SEND_BUTTON = document.querySelector("button[name=send]")
-const CLEAR_BUTTON = document.querySelector("button[id=clearing]")
+const SEND_BUTTON = document.querySelector("button[name=send]");
+
+
 const LEFT_BORDER = -5;
 const RIGHT_BORDER = 3;
+
 let table = document.querySelector("table[id=results]");
 let r = null;
 let x = null;
 let y = null;
 const MAX_ROWS_SAVED = 50;
-window.onbeforeunload = saveTableInLocalStorage
-window.addEventListener('load', loadData);
+window.onbeforeunload = saveTableInLocalStorage;
+window.addEventListener("load", loadData);
 
 function saveTableInLocalStorage() {
     let data = [];
     if (table.rows.length === 1) {
-        console.log("The length of table 1,isn't saved")
         return;
     }
     let rows = table.rows;
     for (let i = 1; i < rows.length && i < MAX_ROWS_SAVED; i++) {
-        let inRange = table.rows[i].cells[0].innerHTML
-        let requestTime = table.rows[i].cells[1].innerHTML
-        let responseTime = table.rows[i].cells[2].innerHTML
-        let x = table.rows[i].cells[3].innerHTML
-        let y = table.rows[i].cells[4].innerHTML
-        let r = table.rows[i].cells[5].innerHTML
-        let temp = {inRange: inRange, requestTime: requestTime, responseTime: responseTime, x: x, y: y, r: r}
+        let inRange = table.rows[i].cells[0].innerHTML;
+        let requestTime = table.rows[i].cells[1].innerHTML;
+        let responseTime = table.rows[i].cells[2].innerHTML;
+        let x = table.rows[i].cells[3].innerHTML;
+        let y = table.rows[i].cells[4].innerHTML;
+        let r = table.rows[i].cells[5].innerHTML;
+        let temp = {
+            inRange: inRange,
+            requestTime: requestTime,
+            responseTime: responseTime,
+            x: x,
+            y: y,
+            r: r,
+        };
         data.push(temp);
     }
-    window.localStorage.setItem('Table', JSON.stringify(data));
-    console.log("Table is saved")
+    window.localStorage.setItem("Table", JSON.stringify(data));
 }
 
 function loadData() {
-    if (localStorage.getItem('Table') === null) {
-        console.log("Table wasn't saved")
+    if (localStorage.getItem("Table") === null) {
         return;
     }
-    let data = JSON.parse(localStorage.getItem('Table'));
-    for (i = data.length > MAX_ROWS_SAVED ? MAX_ROWS_SAVED - 1 : data.length - 1; i >= 0; i--) {
+    let data = JSON.parse(localStorage.getItem("Table"));
+    for (
+        let i = data.length > MAX_ROWS_SAVED ? MAX_ROWS_SAVED - 1 : data.length - 1;
+        i >= 0;
+        i--
+    ) {
         let row = table.insertRow(1);
         let cell1 = row.insertCell(0);
         let cell2 = row.insertCell(1);
@@ -48,110 +58,102 @@ function loadData() {
         let cell4 = row.insertCell(3);
         let cell5 = row.insertCell(4);
         let cell6 = row.insertCell(5);
-        cell1.innerHTML = data[i].inRange
-        cell2.innerHTML = data[i].requestTime
-        cell3.innerHTML = data[i].responseTime
-        cell4.innerHTML = data[i].x
-        cell5.innerHTML = data[i].y
-        cell6.innerHTML = data[i].r
+        cell1.innerHTML = data[i].inRange;
+        cell2.innerHTML = data[i].requestTime;
+        cell3.innerHTML = data[i].responseTime;
+        cell4.innerHTML = data[i].x;
+        cell5.innerHTML = data[i].y;
+        cell6.innerHTML = data[i].r;
     }
-    console.log("Table is refreshed");
-    localStorage.removeItem('Table');
+    localStorage.removeItem("Table");
 }
 
-buttons.forEach(b => {
-    b.addEventListener('click', () => {
-        buttons.forEach(b => b.style.color = 'white')
-        b.style.color = 'red';
-    })
-})
+buttons.forEach((b) => {
+    b.addEventListener("click", () => {
+        buttons.forEach((b) => (b.style.color = "white"));
+        b.style.color = "red";
+    });
+});
 addEventListener("load", () => {
     inputY.value = "";
-    checkBoxes.forEach(box => {
+    checkBoxes.forEach((box) => {
         box.checked = false;
-    })
+    });
     r = null;
     x = null;
     y = null;
-})
-inputY.addEventListener('input', (element) => {
+});
+inputY.addEventListener("input", (element) => {
     element.target.setCustomValidity("");
     const isValid = element.target.checkValidity();
-    let res = inputY.value
+    let res = inputY.value;
     if (!isValid) {
         return;
     }
     if (res < LEFT_BORDER || res > RIGHT_BORDER) {
-        element.target.setCustomValidity("Invalid field.")
+        element.target.setCustomValidity("Invalid field.");
         return;
     }
-
     y = parseFloat(res);
+});
 
-
-})
-
-checkBoxes.forEach(box => {
-    box.addEventListener('change', () => {
+checkBoxes.forEach((box) => {
+    box.addEventListener("change", () => {
         box.checked = true;
         checkBoxes.forEach((item) => {
-            if (item !== box) item.checked = false
-        })
+            if (item !== box) item.checked = false;
+        });
         x = box.value;
-        console.log("x=".concat(x));
+    });
+});
+buttons.forEach((b) => {
+    b.addEventListener("click", () => {
+        r = b.value;
+    });
+});
 
-    })
-})
-buttons.forEach(b => {
-    b.addEventListener('click', () => {
-        r = b.value
-        console.log("r=".concat(r));
-
-    })
-})
-
-
-SEND_BUTTON.addEventListener('click', () => {
+SEND_BUTTON.addEventListener("click", async onfulfilled => {
     if (r === null) {
-        alert("Fill parameter r")
+        alert("Fill parameter r");
         return;
     }
     if (x === null) {
-        alert("Fill parameter x")
+        alert("Fill parameter x");
         return;
     }
     if (y === null) {
-        alert("Fill parameter y")
+        alert("Fill parameter y");
         return;
     }
-    let currentTime = new Date()
-        .toLocaleString("ru-RU", {timeZone: "Europe/Moscow", hour: "numeric", minute: "numeric", second: "numeric"})
+    let currentTime = new Date().toLocaleString("ru-RU", {
+        timeZone: "Europe/Moscow",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+    });
 
+    //  const res = await axios.post('scripts\\script.php', json) ;
+    //  console.log(res.data.data)
+    axios.post('scripts\\script.php', {"x": x, "y": y, "r": r})
+        .then(function (response) {
 
-    $.post("\\scripts\\script.php", {
-        x: x, y: y, r: r
-    }, (result) => {
-        let json = JSON.parse(result)
+            let row = table.insertRow(1);
+            let cell1 = row.insertCell(0);
+            let cell2 = row.insertCell(1);
+            let cell3 = row.insertCell(2);
+            let cell4 = row.insertCell(3);
+            let cell5 = row.insertCell(4);
+            let cell6 = row.insertCell(5);
+            cell1.innerHTML = response.data.boolean;
+            cell2.innerHTML = currentTime
+            cell3.innerHTML = response.data.response_time;
+            cell4.innerHTML = response.data.x;
+            cell5.innerHTML = response.data.y;
+            cell6.innerHTML = response.data.r;
+        })
+});
 
-        let row = table.insertRow(1);
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-        let cell3 = row.insertCell(2);
-        let cell4 = row.insertCell(3);
-        let cell5 = row.insertCell(4);
-        let cell6 = row.insertCell(5);
-        cell1.innerHTML = json.boolean;
-        cell2.innerHTML = currentTime
-        cell3.innerHTML = json.response_time;
-        cell4.innerHTML = json.x;
-        cell5.innerHTML = json.y;
-        cell6.innerHTML = json.r;
-    })
-
-})
-
-
-CLEAR_BUTTON.addEventListener('click', () => {
+function removeAllElements() {
     $("#results").find("tr:gt(0)").remove();
-})
+}
 
--- scripts/script.php
+++ scripts/script.php
@@ -1,15 +1,37 @@
 <?php
 date_default_timezone_set('Europe/Moscow');
-$x = $_POST['x'];
-$y = $_POST['y'];
-$r = $_POST['r'];
+$postData = file_get_contents('php://input');
+$data = json_decode($postData, true);
+$x=$data['x'];
+$y=$data['y'];
+$r=$data['r'];
+$pattern_xy = '/-?[1-5]/';
+$pattern_r = '/[1-4](\.5)?/';
 if (!is_numeric($x)||!is_numeric($y)||!is_numeric($r)){
     http_response_code( 400);
     return;
 }
-if (($x>=0 && $x<=$r/2 && $y<=0 && $y>=-$r/2)||
-    ($x<=0 && $x>=-$r/2 && $y<=0 && $y>=-$r)||
-    ($x<=0 && $x>=-$r && $y>=0 && $y<=$r)){
+if (!preg_match($pattern_xy, $x)) {
+    $arr = array('message' => 'x is invalid.Must be in range [-3,5]', 'x' => $x);
+    http_response_code(400);
+    echo json_encode($arr);
+    return;
+}
+if (!preg_match($pattern_xy, $y)) {
+    $arr = array('message' => 'Y is invalid.Must be in range [-5,3]', 'y' => $y);
+    http_response_code(400);
+    echo json_encode($arr);
+    return;
+}
+if (!preg_match($pattern_r, $r)) {
+    $arr = array('message' => 'R is invalid.Must be in range [1,4] with step 0.5', 'r' => $r);
+    http_response_code(400);
+    echo json_encode($arr);
+    return;
+}
+if (($x >= 0 && $x <= $r / 2 && $y <= 0 && $y >= -$r / 2) ||
+    ($x <= 0 && $x >= -$r / 2 && $y <= 0 && $y >= -$r) ||
+    ($x <= 0 && $x >= -$r && $y >= 0 && $y <= $r)) {
 
     $data = ['x' => $x, 'y' => $y, 'r' => $r, 'boolean' => 'True', 'response_time' => date("H:i:s.") . gettimeofday()["usec"]];
 
